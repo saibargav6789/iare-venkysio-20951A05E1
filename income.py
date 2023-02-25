57d5645bd5b760f944a65f36dbac64dd571500bc
saibargav6789 <106881951+saibargav6789@users.noreply.github.com>
2023-02-25 11:31:28 +0530

    Add files via upload

---
+++ income.py
@@ -0,0 +1,6 @@
+s=float(input("enter annual income:"))
+a=float(input("enter deduction1:"))
+b=float(input("enter deduction2:"))
+c=float(input("enter deduction3:"))
+k=4*s*(1-((a+b+c)/100))
+print("%2f."%k)
